14d68cde1aa3b49e083b143381f51aaa0f4577ec
Hakbar0 <haseeb2000@hotmail.com>
2026-05-02 14:29:07 +0100

    more crap

--- list.py
+++ list.py
@@ -29,6 +29,19 @@ class LinkedList:
         
         return True
     
+    def prepend(self, value):
+        new_node = Node(value)
+
+        if self.length == 0:
+            self.head = new_node
+            self.tail = new_node
+        else:
+            new_node.next = self.head
+            self.head = new_node
+
+        self.length += 1
+        return True
+
     def pop(self):
         if self.length == 0:
             return None
@@ -48,13 +61,15 @@ class LinkedList:
             self.tail = None
         
         return temp
+    
+
+
             
 
 
 
 linked_list = LinkedList(5)
 linked_list.append(6)
+linked_list.prepend(1)
 linked_list.print_list()
-linked_list.pop()
-linked_list.print_list()
-linked_list.pop()
+
