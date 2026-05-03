95e134d8e25190870189128d1a70c78e56620f7c
Hakbar0 <haseeb2000@hotmail.com>
2026-05-03 15:43:43 +0100

    more stuff

--- list.py
+++ list.py
@@ -142,6 +142,16 @@ class LinkedList:
             before = temp
             temp = after
     
+    def find_middle_node(self):
+        slow = self.head
+        fast = self.head
+        
+        while fast is not None and fast.next is not None:
+            slow = slow.next
+            fast = fast.next.next
+        
+        return slow
+    
 
 
             
