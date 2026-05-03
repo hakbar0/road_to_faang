6a26b17ddd90124800499dfeea264bc15ca18466
Hakbar0 <haseeb2000@hotmail.com>
2026-05-03 15:52:06 +0100

    nore crap

--- list.py
+++ list.py
@@ -149,6 +149,18 @@ class LinkedList:
         while fast is not None and fast.next is not None:
             slow = slow.next
             fast = fast.next.next
+    
+    def has_loop(self):
+        slow = self.head
+        fast = self.head
+        
+        while fast.next and fast.next.next:
+            slow = slow.next
+            fast = fast.next.next
+            if slow == fast:
+                return True
+            
+        return False
         
         return slow
     
