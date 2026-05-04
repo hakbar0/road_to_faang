3f5222c990c222b2eb3a458e8849eb9c9b1047b2
Hakbar0 <haseeb2000@hotmail.com>
2026-05-04 17:03:07 +0100

    more stuff

--- doubly_linked_list.py
+++ doubly_linked_list.py
@@ -10,5 +10,26 @@ class DoublyLinkedList:
         self.head = new_node
         self.tail = new_node
         self.length = 1
+    
+    def print_list(self):
+        temp = self.head
+        while temp:
+            print(temp.value)
+            temp = temp.next
+    
+    def append(self, value):
+        new_node = Node(value)
+        if self.head is None:
+            self.head = new_node
+            self.tail = new_node
+        else:
+            self.tail.next = new_node
+            new_node.prev = self.tail
+            self.tail = new_node
+        self.length += 1
+
+        return True
 
 my_doubly_linked_list = DoublyLinkedList(7)
+my_doubly_linked_list.append(2)
+my_doubly_linked_list.print_list()
