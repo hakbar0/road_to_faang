82eeb83cd3d7a083d006e2e1f497e8875f3dcad0
Hakbar0 <haseeb2000@hotmail.com>
2026-05-09 19:53:21 +0100

    more crap

--- stack.py
+++ stack.py
@@ -26,7 +26,20 @@ class Stack:
         self.height += 1
 
     
+    def pop(self):
+        if self.height == 0:
+            return None
+        
+        temp = self.top
+        self.top = self.top.next
+        temp.next = None
+        self.height -=1
+        
+        return temp
+
+    
 
 my_stack = Stack(4)
 my_stack.push(2)
+my_stack.pop()
 my_stack.print_stack()
