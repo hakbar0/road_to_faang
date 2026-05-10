2ccf6453b067a10c0a72ab1cd83332acf47bb4cf
Hakbar0 <haseeb2000@hotmail.com>
2026-05-10 18:04:16 +0100

    more crap

--- queue.py
+++ queue.py
@@ -16,5 +16,30 @@ class Queue:
             print(temp.value)
             temp = temp.next
 
+    def enqueue(self, value):
+        new_node = Node(value)
+        if self.first == None:
+            self.first = new_node
+            self.last = new_node
+        else:
+            self.last.next = new_node
+            self.last = new_node
+        self.length += 1
+    
+    def dequeue(self):
+        if self.length == 0:
+            return None
+        
+        temp = self.first
+        self.first = temp.next
+        temp.next = None
+
+        self.length -=1
+
+        return temp
+
+
 my_queue = Queue(4)
+my_queue.enqueue(2)
+my_queue.dequeue()
 my_queue.print_queue()
