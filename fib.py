3a97a469fe025f4adb8d1cb35fb58d20cd5ca3a5
Hakbar0 <haseeb2000@hotmail.com>
2026-05-16 12:27:03 +0100

    more crap

---
+++ fib.py
@@ -0,0 +1,15 @@
+counter = 0
+
+def fib(n):
+
+    global counter
+    counter += 1
+    if n == 0 or n == 1:
+        return n
+    
+    return fib(n - 1) + fib(n - 2)
+
+n = 35
+
+fib(n)
+print("counter", counter)
---
+++ fib_optimised.py
@@ -0,0 +1,12 @@
+memo = [None] * 100
+
+def fib(n):
+    if memo[n] is not None:
+        return memo[n]
+    
+    if n == 0 or n == 1:
+        return n
+    
+    memo[n] = fib(n - 1) + fib(n - 2)
+
+    return memo[n]
